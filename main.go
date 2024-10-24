57b95e0611d90cfab9f962356ff9359a5ed1279a
Gulumuru Vinodh Kumar <vgulumuru@altimetrik.com>
2024-10-24 18:22:21 +0530

    Added logs

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Println("inside the hello controller")
+	fmt.Fprintf(w, "hello go func. It's a router \n")
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
 
 func main() {
-	fmt.Println("Hello... This go func")
+
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	fmt.Println("router successfully started")
+	http.ListenAndServe(":8090", nil)
 }
+
+// func main() {
+// 	fmt.Println("Hello... This go func")
+// }
